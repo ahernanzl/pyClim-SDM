3212ce4866a6a1d362f622a08bad948b60141d43
ahernanzl@aemet.es <ahernanzl@aemet.es>
2022-03-03 14:58:39 +0000

    Fixed minor bug

--- lib/ANA_lib.py
+++ lib/ANA_lib.py
@@ -423,7 +423,6 @@ def coefficients_collect_chunks(var, methodName, mode, nproc=1):
         filename = path + 'intercept_ichunk_' + str(ichunk) + '.npy'
         intercept = np.append(intercept, np.load(filename), axis=0)
     shutil.rmtree(path)
-    shutil.rmtree(path)
 
     np.save(pathOut+var+'_'+methodName+'_coefficients', coef)
     np.save(pathOut+var+'_'+methodName+'_intercept', intercept)
