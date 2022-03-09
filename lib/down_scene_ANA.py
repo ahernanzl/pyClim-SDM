1ab17764dc8f4c127025bfc0df6d1abbc2c69129
ahernanzl@aemet.es <ahernanzl@aemet.es>
2022-03-09 09:54:09 +0000

    Updated README.md

--- lib/down_scene_ANA.py
+++ lib/down_scene_ANA.py
@@ -15,7 +15,6 @@ import down_scene_TF
 import down_scene_WG
 import down_day
 import down_point
-import evaluate_GCMs
 import evaluate_methods
 import grids
 import launch_jobs
@@ -23,6 +22,7 @@ import plot
 import postpro_lib
 import postprocess
 import derived_predictors
+import precontrol
 import preprocess
 import process
 import read
