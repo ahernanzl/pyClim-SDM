af6a7ff866a5f9e16ced2cffc1f394bba6262e06
ahernanzl@aemet.es <ahernanzl@aemet.es>
2022-08-10 11:39:34 +0000

    minor improvements

--- install_requirements.py
+++ install_requirements.py
@@ -1,5 +1,5 @@
 import os
-f = open('requirements_NEW.txt', 'r')
+f = open('requirements.txt', 'r')
 for line in f.readlines():
     print(line)
     os.system('conda install -c conda-forge ' + line)
