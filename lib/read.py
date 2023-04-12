df6e5768e4f062286bcd51584068142c3a1410f6
ahernanzl@aemet.es <ahernanzl@aemet.es>
2023-04-12 10:28:45 +0000

    Fixed small bug reading netCDF dates

--- lib/read.py
+++ lib/read.py
@@ -287,10 +287,10 @@ def lres_data(targetVar, field, grid=None, model='reanalysis', scene=None, predN
     else:
         datesDefined = False
         for pred in preds_dict[targetVar]:
-            if len(pred) > 4 and pred[-4:] in all_levels:
-                level = pred[-4:]
-            elif len(pred) > 3 and pred[-3:] in all_levels:
-                level = pred[-3:]
+            if len(pred) > 4 and pred[-4:] in [str(x) for x in all_levels]:
+                level = int(pred[-4:])
+            elif len(pred) > 3 and pred[-3:] in [str(x) for x in all_levels]:
+                level = int(pred[-3:])
             else:
                 level = None
             try:
@@ -300,11 +300,11 @@ def lres_data(targetVar, field, grid=None, model='reanalysis', scene=None, predN
                 break
             except:
                 pass
-            if datesDefined == False:
-                print('ERROR retrieving dates from netCDF models files')
-                print('Make sure your input_data/models directory contains the needed files.')
-                print('At least one direct predictors is needed, not only derived predictors.')
-                exit()
+        if datesDefined == False:
+            print('ERROR retrieving dates from netCDF models files')
+            print('Make sure your input_data/models directory contains the needed files.')
+            print('At least one direct predictors is needed, not only derived predictors.')
+            exit()
 
     ndates = len(dates)
 
