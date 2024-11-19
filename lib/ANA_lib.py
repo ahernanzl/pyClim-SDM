dfce9af2062313702223478488694f3849e40e88
ahernanzl@aemet.es <ahernanzl@aemet.es>
2024-11-19 11:02:48 +0000

    dev ahl

--- lib/ANA_lib.py
+++ lib/ANA_lib.py
@@ -45,7 +45,6 @@ def set_number_of_weather_types():
 	Some parameters of both MiniBatchKmeans and Kmeans might need to be tuned.
 	"""
 
-
 	print('weather_types: set number of clusters')
 	# Define clustering method
 	# clustering_method = 'Kmeans'
