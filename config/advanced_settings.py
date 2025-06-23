1133a57ba4e685f9ed174736b3a28bc28767071f
ahernanzl@aemet.es <ahernanzl@aemet.es>
2025-06-23 10:46:36 +0000

    Force calculate downscaling climdex and bias correction

--- config/advanced_settings.py
+++ config/advanced_settings.py
@@ -232,9 +232,9 @@ plot_hyperparameters_epochs_nEstimators_featureImportances = False
 mean_and_std_from_GCM = True
 
 # Force calculations even if files already exist
-force_downscaling = False
-force_climdex_calculation = False
-force_bias_correction = False
+force_downscaling = True
+force_climdex_calculation = True
+force_bias_correction = True
 
 exp_var_ratio_th = .95  # threshold for PCA of SAFs
 k_clusters = 250  # set to None first time, and when weather_types.set_number_of_clusters ends see elbow curve and
